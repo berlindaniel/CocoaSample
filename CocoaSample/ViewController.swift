6f29e3360cdcd04de03c5a056cea829289a6a90c
Daniel Berlin <danielberlin@Daniels-MacBook-Pro-2.local>
2018-10-21 18:06:33 -0400

    First commit

--- CocoaSample/ViewController.swift
+++ CocoaSample/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
-
+import TextFieldEffects
+import FSCalendar
 class ViewController: UIViewController {
 
+    @IBOutlet var myCalendar: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        let textFieldFrame = CGRect(x: self.view.center.x, y: self.view.center.y, width: 200, height: 50)
+//        let textField = JiroTextField(frame: textFieldFrame)
+//        textField.placeholderColor = .darkGray
+//        textField.backgroundColor = .lightGray
+//        textField.placeholder = "Username"
+//
+//        let passFieldFrame = CGRect(x: self.view.center.x, y: self.view.center.y + 75, width: 200, height: 50)
+//        let passField = JiroTextField(frame: passFieldFrame)
+//        passField.placeholderColor = .darkGray
+//        passField.backgroundColor = .lightGray
+//        passField.placeholder = "Password"
+//
+//
+//        self.view.addSubview(textField)
+//        self.view.addSubview(passField)
     }
 
+    
 
 }
 
